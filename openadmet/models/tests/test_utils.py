c49acf0c15f243339b483fab56c11691c02972e8
hmacdope <hugomacdermott@gmail.com>
2025-04-21 13:34:19 +1000

    add CLI testing and requsite data

---
+++ openadmet/models/tests/test_utils.py
@@ -0,0 +1,9 @@
+import traceback
+
+def click_success(result):
+    if result.exit_code != 0:  # -no-cov-  (only occurs on test error)
+        print(result.output)
+        traceback.print_tb(result.exc_info[2])
+        print(result.exc_info[0], result.exc_info[1])
+    return result.exit_code == 0
+
