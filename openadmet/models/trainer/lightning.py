221242a27e00c3b4daa768765a5abafe57776a39
hmacdope <hugomacdermott@gmail.com>
2025-05-26 13:41:47 +1000

    fix merge conflict issue

--- openadmet/models/trainer/lightning.py
+++ openadmet/models/trainer/lightning.py
@@ -71,11 +71,7 @@ class LightningTrainer(TrainerBase):
         """
 
         # Indicate that the model is being trained
-<<<<<<< HEAD
         logger.debug(f"Training model {self.model._estimator}")
-=======
-        logger.info(f"Training model {self.model._estimator}")
->>>>>>> origin/main
 
         # Fit model
         self._trainer.fit(self.model._estimator, train_dataloader, val_dataloader)
