791265dbe6d207196580e94aa1433e1b89140dd3
Devany West <31937638+dwwest@users.noreply.github.com>
2025-10-08 20:06:15 -0400

    Bugfixes and updates to posthoc comparison to streamline user experience (#300)
    * Add model-dir to replace model-stats file
    
    * Add label and taskname from anvil function
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add function to read in anvil recipes and cross validation metrics from a parent folder
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add chemeleon special case for model labeling
    
    * Allows posthoc to now cleanly fail if multiple identical labels are provided for comparison
    
    * Fix error with path vs Path for click in compare cli
    
    * Fix spelling mistake in compare cli
    
    * Update tests with new arg names
    
    * Add better unit testing for posthoc
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add documentation and default types to functions in comparison
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add featurizer label to be read from anvil plus testing
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix color issues, fix cli to add new arguments, clean up docstrings, rename arguments to be consistent between cli and posthoc code
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix comment issues after merge
    
    * Add preliminary function for reading from s3 buckets
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add boto3 to the conda install
    
    * Add integration test for comparison
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix argument name in posthoc unit tests
    
    * Fix argument names in posthoc tests again
    
    * Fix issue in posthoc anvil integration test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix anvil test tmp_folder issue
    
    * Fix posthoc integration test argument calls
    
    * Fix posthoc testing issues with incorrect filepaths
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add comments for posthoc testing
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add Sean's fixes
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- openadmet/models/cli/compare.py
+++ openadmet/models/cli/compare.py
@@ -7,22 +7,41 @@ from openadmet.models.comparison.posthoc import PostHocComparison
 
 @click.command()
 @click.option(
-    "--model-stats",
-    multiple=True,
-    help="Path to JSON of model stats, needst to be in the same order as model-tag",
-    required=True,
+    "--model-dirs",
+    help="Path to main model directory",
+    required=False,
     type=click.Path(exists=True),
-)
-@click.option(
-    "--model-tag",
-    help="Names to identify different models, user specified in same order as model-stats",
     multiple=True,
 )
 @click.option(
-    "--task-name",
+    "--label-types",
+    help="Category from the yaml file with which to label each model",
+    required=False,
+    multiple=True,
+)
+@click.option(
+    "--model-stats-fns",
+    help="File paths to JSON cross validation files with model statistics",
+    required=False,
+    multiple=True,
+)
+@click.option(
+    "--labels",
+    help="User-defined labels for each model",
+    required=False,
+    multiple=True,
+)
+@click.option(
+    "--task-names",
     help="Task names as they appear in the model stats JSON",
-    required=True,
+    required=False,
     multiple=True,
+)
+@click.option(
+    "--mt-id",
+    help="If using label and multitask, give the name of the target or other identifier",
+    required=False,
+    multiple=False,
 )
 @click.option(
     "--output-dir",
@@ -37,32 +56,56 @@ from openadmet.models.comparison.posthoc import PostHocComparison
     type=bool,
 )
 def compare(
-    model_stats,
-    model_tag,
-    task_name,
+    model_dirs,
+    label_types,
+    model_stats_fns,
+    labels,
+    task_names,
+    mt_id=None,
     output_dir=None,
     report=False,
 ):
     """
     Compare two or more models from summary statistics.
 
+    Either (`model_dirs` and `label_types`) OR (`model_stats_fns`, `labels`, and `task_names`) are required.
+    If the full anvil directory is available, the first option is recommended. If only the JSON files
+    with model statistics are available, use the second option.
+
     Parameters
     ----------
-    model_stats : list
-        Paths to JSON files of model statistics, need to be in the same order as model_tag
-    model_tag : list
-        Names to identify different models, user specified in same order as model_stats
-    task_name : list
-        Task names as they appear in the model stats JSON
+    model_dirs : list of str, optional
+        List of paths to main model directories.
+    label_types : list of str, optional
+        List of categories from the `anvil_recipe.yaml` file to use for labeling each model.
+        Supported values are 'biotarget', 'model', 'feat', and 'tasks'.
+    model_stats_fns : list of str, optional
+        List of file paths to JSON files containing model statistics.
+    labels : list of str, optional
+        User-defined list of tags for the models, used for plotting and reporting.
+    task_names : list of str, optional
+        List of task names as they appear in the model statistics JSON files.
+    mt_id : str, optional
+        Identifier for the target column when comparing multitask models. Used to select
+        the appropriate task from the `anvil_recipe.yaml` file.  Must be a unique
+        string not appearing in any target columns for other models in the file. Required if comparing
+        multitask models.
     output_dir : str, optional
-        Path to output directory, by default None
+        Path to output directory.
     report : bool, optional
-        Whether to write summary pdf to output-dir, by default False
+        Whether to generate a PDF report of the comparison results. Default is False.
 
     """
     comp = PostHocComparison()
     comp.compare(
-        model_stats, model_tag, task_name, output_dir=output_dir, report=report
+        model_dirs,
+        label_types,
+        model_stats_fns,
+        labels,
+        task_names,
+        mt_id=mt_id,
+        output_dir=output_dir,
+        report=report,
     )
 
 
