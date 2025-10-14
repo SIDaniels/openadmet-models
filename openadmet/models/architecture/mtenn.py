f1f6df79915eee449a56e887fd74733bc6d44383
Kate Huddleston <37937527+khuddzu@users.noreply.github.com>
2025-10-13 23:23:14 -0500

    Create a warning if MTENN is being used with CPU (#386)
    * Add gpu dependency to predict
    
    * Change error message to warning
    
    * Make warning more obnoxious
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add line to satisfy linter
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add line to satisfy linter
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * try new approach to satify linter
    
    * Add line after removal (even though all other functions dont have a blank line) to satisfy linter
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add blank line before warning
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update warning to env variable
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add new variable to predict docstring
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- openadmet/models/architecture/mtenn.py
+++ openadmet/models/architecture/mtenn.py
@@ -10,6 +10,8 @@ from mtenn.config import ModelConfig, SchNetRepresentationConfig
 from openadmet.models.architecture.model_base import LightningModelBase
 from openadmet.models.architecture.model_base import models as model_registry
 
+import os
+
 
 class MTENNLightningModule(pl.LightningModule):
     """
@@ -248,6 +250,9 @@ class MTENNSchNetModel(LightningModelBase):
         """
         Use the model for prediction.
 
+        WARNING: Current MTENN issue where loading a model must be done on GPU to obtain the correct weights.
+        Set env variable OADMET_ALLOW_MTENN_CPU to either 1 or True to use CPU.
+
         Parameters
         ----------
         dataloader : torch.utils.data.DataLoader
@@ -270,6 +275,13 @@ class MTENNSchNetModel(LightningModelBase):
         """
         if not self.estimator:
             raise AttributeError("Model not built or trained.")
+
+        if accelerator == "cpu" and not os.getenv(
+            "OADMET_ALLOW_MTENN_CPU", ""
+        ).lower() in {"1", "true"}:
+            raise ValueError(
+                "CPU inference currently disabled for MTENN. If you must use CPU, set env var OADMET_ALLOW_MTENN_CPU to True. GPU trained weights loaded on CPU will give incorrect predictions."
+            )
 
         with torch.inference_mode():
             trainer = pl.Trainer(
