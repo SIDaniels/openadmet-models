38a356f057c85d58f201c2ff92198bc3a7ba8657
Devany West <31937638+dwwest@users.noreply.github.com>
2025-11-12 11:09:15 -0500

    Add cluster splitters to split (#431)
    * Add datamol kmeans splitter to split
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Added file header for ruff check
    
    * Add Bemis Murcko and Butina splitters
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add cluster splitter with three methods + tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Changed to correct branch name for uru pip install
    
    * Changed to correct branch name for uru pip install gpu
    
    * Add more data to test for clustering so all clusters have >1 compounds
    
    * Add rdkit utils package to requirements.yaml
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ openadmet/models/split/cluster.py
@@ -0,0 +1,113 @@
+"""Cluster-based data splitting implementations."""
+
+from pydantic import BaseModel, field_validator
+from typing import Literal
+from sklearn.model_selection import train_test_split
+from splito import KMeansSplit
+import numpy as np
+import pandas as pd
+from openadmet.models.split.split_base import SplitterBase, splitters
+from useful_rdkit_utils import (
+    get_kmeans_clusters,
+    get_butina_clusters,
+    get_bemis_murcko_clusters,
+)
+
+
+@splitters.register("ClusterSplitter")
+class ClusterSplitter(SplitterBase):
+    """Splits the data based on the KMeans clustering of the molecules."""
+
+    method: str = "butina"
+    k_clusters: int = 10
+
+    @field_validator("method", mode="before")
+    @classmethod
+    def validate_method(cls, value):
+        """Validate that the method is one of the allowed options."""
+        if value not in {"butina", "kmeans", "bemis-murcko"}:
+            raise ValueError(
+                f"Invalid method: {value}. Must be one of 'butina', 'kmeans', or 'bemis-murcko'."
+            )
+        return value
+
+    def split(self, X, y):
+        """
+        Split the data into train, validation, and test sets.
+
+        Parameters
+        ----------
+        X : Iterable[str]
+            List or iterable of SMILES strings to split.
+        y : Iterable[float] or pd.Series
+            List or iterable of target values corresponding to the SMILES strings.
+
+        Returns
+        -------
+        tuple
+            Tuple containing:
+            - X_train: Training set SMILES strings.
+            - X_val: Validation set SMILES strings (or None if val_size=0).
+            - X_test: Test set SMILES strings (or None if test_size=0).
+            - y_train: Training set target values.
+            - y_val: Validation set target values (or None if val_size=0).
+            - y_test: Test set target values (or None if test_size=0).
+
+        """
+        # Get clusters based on the selected method
+        if self.method == "butina":
+            clusters = get_butina_clusters(X)
+        elif self.method == "bemis-murcko":
+            clusters = get_bemis_murcko_clusters(X)
+        elif self.method == "kmeans":
+            clusters = get_kmeans_clusters(X, n_clusters=self.k_clusters)
+
+        # No test set requested
+        if self.test_size == 0:
+            # Split into train and val
+            X_train, X_val, y_train, y_val = train_test_split(
+                X,
+                y,
+                train_size=None,
+                test_size=int(self.val_size * X.shape[0]),
+                random_state=self.random_state,
+                stratify=clusters,
+            )
+            return X_train, X_val, None, y_train, y_val, None
+
+        # Split into train+val and test
+        X_train_val, X_test, y_train_val, y_test = train_test_split(
+            X,
+            y,
+            train_size=None,
+            test_size=int(self.test_size * X.shape[0]),
+            random_state=self.random_state,
+            stratify=clusters,
+        )
+
+        # No validation set requested, return train(+val) and test sets
+        if self.val_size == 0:
+            return X_train_val, None, X_test, y_train_val, None, y_test
+
+        # Get new clusters based on the selected method
+        if self.method == "butina":
+            split_clusters = get_butina_clusters(X_train_val)
+        elif self.method == "bemis-murcko":
+            split_clusters = get_bemis_murcko_clusters(X_train_val)
+        elif self.method == "kmeans":
+            split_clusters = get_kmeans_clusters(
+                X_train_val, n_clusters=self.k_clusters
+            )
+
+        # Split train+val into train and val sets
+        X_train, X_val, y_train, y_val = train_test_split(
+            X_train_val,
+            y_train_val,
+            train_size=None,
+            test_size=int(self.val_size * X.shape[0]),
+            random_state=self.random_state,
+            stratify=split_clusters,
+        )
+
+        # Return train, val and test sets
+        return X_train, X_val, X_test, y_train, y_val, y_test
--- openadmet/models/tests/unit/split/test_splitters.py
+++ openadmet/models/tests/unit/split/test_splitters.py
@@ -1,12 +1,16 @@
 import numpy as np
 import pytest
+import pandas as pd
 
 from openadmet.models.split.sklearn import ShuffleSplitter
+from openadmet.models.split.cluster import ClusterSplitter
 from openadmet.models.split.split_base import splitters
+from openadmet.models.tests.unit.datafiles import CYP3A4_chembl_pchembl
 
 
 def test_in_splitters():
     assert "ShuffleSplitter" in splitters
+    assert "ClusterSplitter" in splitters
 
 
 @pytest.mark.parametrize(
@@ -50,6 +54,93 @@ def test_simple_split(
     # Generate random data
     X = np.random.rand(100, 10)
     y = np.random.rand(100)
+
+    # Error is expected
+    if error is True:
+        with pytest.raises(ValueError):
+            splitter.split(X, y)
+        return
+
+    # Perform the split
+    X_train, X_val, X_test, y_train, y_val, y_test = splitter.split(X, y)
+
+    # Check train
+    assert X_train.shape[0] == expected_train
+    assert y_train.shape[0] == expected_train
+
+    # Validation set requested
+    if val_size > 0:
+        assert X_val.shape[0] == expected_val
+        assert y_val.shape[0] == expected_val
+
+    # Validation set not requested
+    else:
+        assert X_val is None
+        assert y_val is None
+
+    # Test set requested
+    if test_size > 0:
+        assert X_test.shape[0] == expected_test
+        assert y_test.shape[0] == expected_test
+
+    # Test set not requested
+    else:
+        assert X_test is None
+        assert y_test is None
+
+
+@pytest.mark.parametrize(
+    "train_size, val_size, test_size, expected_train, expected_val, expected_test, error",
+    [
+        # 80, 0, 20
+        (0.8, 0.0, 0.2, 800, 0, 200, False),
+        # 70, 30, 0
+        (0.7, 0.3, 0.0, 700, 300, 0, False),
+        # 70, 10, 20
+        (0.7, 0.1, 0.2, 700, 100, 200, False),
+        # 60, 20, 20
+        (0.6, 0.2, 0.2, 600, 200, 200, False),
+        # 100, 0, 0; raises error
+        (1.0, 0.0, 0.0, -1, -1, -1, True),
+        # 50, 50, 50; raises error
+        (0.5, 0.5, 0.5, -1, -1, -1, True),
+    ],
+)
+def test_cluster_split(
+    train_size,
+    val_size,
+    test_size,
+    expected_train,
+    expected_val,
+    expected_test,
+    error,
+):
+    df = pd.read_csv(CYP3A4_chembl_pchembl)
+    X = df["CANONICAL_SMILES"].values[:1000]
+    y = df["pChEMBL mean"].values[:1000]
+
+    # Error expected
+    if error is True:
+        with pytest.raises(ValueError):
+            # Initialize splitter
+            splitter = ClusterSplitter(
+                train_size=train_size,
+                val_size=val_size,
+                test_size=test_size,
+                random_state=42,
+                k_clusters=2,
+            )
+        return
+
+    # Initialize splitter
+    splitter = ClusterSplitter(
+        train_size=train_size,
+        val_size=val_size,
+        test_size=test_size,
+        random_state=42,
+        method="kmeans",
+        k_clusters=2,
+    )
 
     # Error is expected
     if error is True:
