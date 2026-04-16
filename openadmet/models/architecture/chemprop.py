37a82b85ead348d3340e7c3006e207d0c51268c2
Devany West <devanywest314@gmail.com>
2026-04-16 15:42:32 -0400

    Trying to fix

--- openadmet/models/architecture/chemprop.py
+++ openadmet/models/architecture/chemprop.py
@@ -447,17 +447,11 @@ class ChemPropModel(LightningModelBase):
                     logger.warning(
                         "Using CheMeleon overrides settings for depth, message_hidden_dim, messages, and aggregation"
                     )
-                elif self.foundation_path:
+                else:
                     logger.info(f"Loading foundation model from {self.foundation_path}")
                     model_path = Path(self.foundation_path)
                     if not model_path.exists():
-<<<<<<< HEAD
-                        raise FileNotFoundError(
-                            f"Foundation model not found at {model_path}"
-                        )
-=======
                         raise FileNotFoundError(f"Foundation model not found at {model_path}")
->>>>>>> c386770 (Removed multi-case handler, changed to script to modify chemprop pt files to get weights saved instead)
                 foundation_mp = torch.load(model_path, weights_only=False)
                 aggr = nn.MeanAggregation()
                 mp = nn.BondMessagePassing(**foundation_mp["hyper_parameters"])
